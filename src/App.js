ddaeea24590583a039bde8691dcca7eb0bc1e182
Anas7866 <75615801+Anas7866@users.noreply.github.com>
2021-09-11 00:53:11 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
        <NavItem icon={<i style={{fontSize:"190%"}} class="fas fa-bell"></i>} />
        <NavItem icon={<i style={{fontSize:"190%"}} class="fas fa-bell"></i>} />
        <NavItem icon={<i style={{fontSize:"190%"}} class="fas fa-bell"></i>} />
+       <NavItem icon={<i style={{fontSize:"190%"}} class="fas fa-bell"></i>}>
+         <DropdownMenu/>
+       </NavItem>    
      </Navbar>
     </div>
   );
@@ -19,7 +22,7 @@ function App() {
 
 function Navbar( props ) {
   return (
-    <nav className="navbar">
+    <nav className="navbar"> 
       <ul className="navbar-nav">
         {props.children}
       </ul>
@@ -30,14 +33,54 @@ function Navbar( props ) {
 }
 
 function NavItem(props) {
-  const [open, setOpen] = useState();
+  const [open, setOpen] = useState(false);
   return (
-    <div className="nav-item">
-     <a herf="#" className="icon-button">
+    <li className="nav-item">
+     <a herf="#" className="icon-button" onClick={()=> setOpen(!open)}>
       {props.icon}
      </a>
-    </div>
+     {open && props.children}
+    </li>
+
+    
   );
 }
+
+function DropdownMenu() {
+
+
+  function DropdownItem(props) {
+    
+    return (
+     <a herf="#" className="menu-item">
+     <span className="icon-button">{props.leftIcon}</span>
+     <span>  {props.children} </span>
+       <span className="icon-right">{props.rightIcon}</span>
+     </a>
+      
+    );
+  }
+  
+ 
+  return (
+    <div className="dropdown">
+      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem
+      leftIcon={<i class="fas fa-bell"></i>}
+      >
+
+      </DropdownItem>
+      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem
+      rightIcon={<i class="fas fa-bell"></i>}
+      >
+
+      </DropdownItem>
+    </div>
+
+    
+  );
+}
+
 
 export default App;
